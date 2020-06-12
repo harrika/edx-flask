a53b121e430e0a25d17f9ef4a99ec17b40d8abb7
harrika <henry.kirya@gmail.com>
2020-06-12 21:05:12 +0300

    conclude on flask sessions

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ from flask_session import Session
 # from data import nem
 
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] == False
-app.config["SESSION_TYPE"] == "filesystem"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # names=nem()
